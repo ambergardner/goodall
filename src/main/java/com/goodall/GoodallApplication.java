2ddaa6753223c1763e568f463ee5ec984ff32882
Nathan Butler <nathanbutler@Nathans-MacBook-Pro.local>
2017-04-04 15:59:52 -0500

    setup

---
+++ src/main/java/com/goodall/GoodallApplication.java
@@ -0,0 +1,12 @@
+package com.goodall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodallApplication.class, args);
+	}
+}
